ee87493e3a6e6948038df2bb12b02945b679178c
shiro-1215 <ee227100@meiji.ac.jp>
2024-10-04 23:23:14 +0900

    usersモジュールの作成

---
+++ src/users/users.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from "@nestjs/common";
+import { InjectModel } from "@nestjs/sequelize";
+import { User } from "src/models/user.model";
+
+@Injectable()
+export class UsersService {
+    constructor(
+        @InjectModel(User)
+        private userModel: typeof User,
+    ){
+        console.log("UsersServiceのコンストラクタ呼び出し");
+    }
+
+    async findAll(): Promise<User[]> {
+        return this.userModel.findAll();
+    }
+}
