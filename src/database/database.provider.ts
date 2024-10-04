cb6279d1c6b721b2d9b9d721f3a6f89fa9449cc6
shiro-1215 <ee227100@meiji.ac.jp>
2024-10-05 00:04:08 +0900

    sequelizeインスタンス生成とデータベースへの同期ができていなかったので追加

--- src/database/database.provider.ts
+++ src/database/database.provider.ts
@@ -1,11 +1,11 @@
 'use strict';
 
 import { SequelizeModule } from '@nestjs/sequelize';
-import { User } from './models/user.model';
-import { Task } from './models/task.model';
-import { Category } from './models/category.model';
+import { User } from '../models/user.model';
+import { Task } from '../models/task.model';
+import { Category } from '../models/category.model';
 
-export const databaseModule = SequelizeModule.forRoot({
+export const databaseProvider = SequelizeModule.forRoot({
   dialect: 'sqlite',
   host: 'localhost',
   port: 3306,
